f45ee8ceb59c10a1c94eb3dadd77ed4ae2db0b4e
dcmurata <n.murata@g.dreamcareer.co.jp>
2022-04-21 18:40:32 +0900

    初期ロード時に表示する値を追加

--- app/routes/organization/home.js
+++ app/routes/organization/home.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const list = require("../../src/tasks/list.js");
 
-router.get('/', function (req, res, next) {
+
+router.get('/', async function (req, res, next) {
     if (req.session.organization_id) {
+
+        const listTasks = await list.getListTasks();
+
         res.render('organization/home', {
-            title: '管理者ホーム'
+            title: '管理者ホーム',
+            sid: req.session.organization_id,
+            category: listTasks.categories
         });
     } else {
         res.redirect('login');
