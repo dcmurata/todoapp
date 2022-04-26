e13eac3feb0673a20b6f3dac4905079fc651b88b
dcmurata <n.murata@g.dreamcareer.co.jp>
2022-04-26 09:59:56 +0900

    ユーザ一覧、タスク一覧を取得する

--- app/src/organization/list.js
+++ app/src/organization/list.js
@@ -7,17 +7,33 @@ getListOrganizations = async function (id) {
     try {
         connection = await mysql.createConnection(config.dbSetting);
         const sql =
-            `SELECT et_usr.id, t_user.user_name 
-             FROM t_user LEFT JOIN t_user_management 
-                ON t_user.id = t_user_management.user_id
-                INNER JOIN t_organization ON t_user_management.organization_id = t_organization.id
-                WHERE t_organization.id = ?`;
+            `SELECT t_user.id AS user_id, t_user.user_name,um.id AS um_id
+             FROM t_user
+                LEFT JOIN t_user_management um
+                    ON t_user.id = um.user_id
+                INNER JOIN t_organization
+                    ON um.organization_id = t_organization.id
+             WHERE t_organization.id = ?`;
         let d = [id];
         const [ousers, fields] = await connection.query(sql, d);
-        // const sql2 = "SELECT * FROM m_category";
-        // const [categories, fields2] = await connection.query(sql2, d);
-        // const list = { tasks: tasks, categories: categories };
-        return ousers;
+
+        const sql2 =
+            `SELECT t_task.*,um.id AS um_id
+             FROM t_task
+                INNER JOIN t_task_management tm
+                    ON t_task.id = tm.task_id
+                INNER JOIN t_user_management um
+                    ON tm.user_management_id = um.id
+             WHERE um.organization_id = ?`;
+
+        const [tasks, fields2] = await connection.query(sql2, d);
+
+        const sql3 = "SELECT * FROM m_category";
+        d = [];
+        const [categories, fields3] = await connection.query(sql3, d);
+
+        const list = { ousers: ousers, tasks: tasks, categories: categories };
+        return list;
     } catch (err) {
         console.log(err);
     } finally {
